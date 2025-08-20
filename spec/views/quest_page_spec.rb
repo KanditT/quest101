f3573eb4110db13c06ddf7830bca4e7baf5c96de
KanditT <kandit.t@kkumail.com>
2025-08-20 10:24:37 +0700

    fix: test

---
+++ spec/views/quest_page_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Root Path', type: :request do
+  describe 'GET /' do
+    let!(:quest) { Quest.create!(name: 'Learn RSpec') } # rubocop:disable RSpec/LetSetup
+
+    it 'renders the root page successfully' do # rubocop:disable RSpec/MultipleExpectations,RSpec/ExampleLength
+      get root_path
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include('My Academy Quest')
+      expect(response.body).to include('Let&#39;s add more quest...')
+      expect(response.body).to include('Create Quest')
+      expect(response.body).to include('Learn RSpec')
+    end
+  end
+end
